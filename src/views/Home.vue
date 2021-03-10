289d590a957d33959c812bd1a375569eb1d62e86
hk-rks <hatsune.kitajima@rakus.co.jp>
2021-03-10 15:45:49 +0900

    vuetify導入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <v-container>
     <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
